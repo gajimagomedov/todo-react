38c3a0f7e848474fa7bbb3f9e0ba44f4e3a2a46b
gadjimagomedov <islam7581@mail.ru>
2017-10-26 17:27:28 +0300

    массив задач

---
+++ src/todos.js
@@ -0,0 +1,18 @@
+const todos = [
+    {
+        title: "Изучить JavaScript",
+        completed: true
+    },
+
+    {
+        title: "Изучить React",
+        completed: false
+    },
+
+    {
+        title: "Написать приложение",
+        completed: false
+    }
+];
+
+export default todos;
