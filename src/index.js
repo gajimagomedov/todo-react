3c4ff5f34271f843e11be2f62b6a2b2b076c0176
gadjimagomedov <islam7581@mail.ru>
2017-10-27 01:55:29 +0300

    незначительные изменения

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import todos from './todos';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialData={todos} />, document.getElementById('root'));
 registerServiceWorker();
