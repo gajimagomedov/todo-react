821f5940c4dc8cc09d048164126e00b460aecc58
gadjimagomedov <islam7581@mail.ru>
2017-10-27 14:25:26 +0300

    добавлены компаненты Header и Todo . в this.state прописан массив с изначальным состоянием задач

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {PropTypes} from 'prop-types';
 import './App.css';
+import Header from './components/Header';
+import Todo from './components/Todo';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todos: this.props.initialData
+    };
+
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleStatusChange(id){
+
+   let todos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    this.setState({
+      todos: todos
+    })
+  }
+
+  handleDelete(id){
+    let todos = this.state.todos.filter(function(todo){
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos: todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <Header title={'React-Todo'} todos={this.state.todos} />
+        <section className = "todo-list">
+
+          {this.state.todos.map(todo => 
+              <Todo key={todo.id} 
+                    id={todo.id}
+                    title={todo.title} 
+                    completed={todo.completed} 
+                    onStatusChange={this.handleStatusChange}
+                    onDelete={this.handleDelete}
+              />)}
+        
+        </section>
+      </main>
     );
   }
 }
+
+App.propTypes = {
+  initialData: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+  })).isRequired
+};
 
 export default App;
