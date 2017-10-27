1818f22b9f1b28ca4337ca43a2d33b76f5c8b02d
gadjimagomedov <islam7581@mail.ru>
2017-10-27 18:50:05 +0300

    сделали компоненту контролируемой

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,40 @@ import Button from './Button';
 class Form extends Component {
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            title: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange =this.handleChange.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        
+        let title = this.state.title;
+
+        if(title){
+            this.props.onAdd(title);
+            this.setState({title});
+        }
+    }
+
+    handleChange(e){
+
+        let title = e.target.value;
+
+        this.setState({title});
     }
 
     render(){
         return(
-            <form className = "todo-form">
-                <input type="text" placeholder="Что нужно сделать" />
+            <form className = "todo-form" onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Что нужно сделать" 
+                    onChange={this.handleChange} 
+                    value={this.state.title} />
                 <Button type="submit" text="">Добавить</Button>
             </form>
         );
@@ -19,7 +46,7 @@ class Form extends Component {
 }
 
 Form.propTypes = {
-
+    onAdd: PropTypes.func
 }
 
 export default Form;
