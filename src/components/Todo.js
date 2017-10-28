ff41d66f69ccb9d4c761ae22ad066bca25d2f65a
gadjimagomedov <islam7581@mail.ru>
2017-10-28 13:27:50 +0300

    добавили компоненте Todo состояние editing, преобра·овали его в класс

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 import Checkbox from './Checkbox';
+import Button from './Button';
 
-function Todo(props){
-    return(
-        <div className="todo">
-            <Checkbox checked={props.completed} />
 
-            <span className="todo-title">{props.title}</span>
+class Todo extends Component {
+    constructor(props){
+        super(props)
 
-            <button className="delete icon">
-                <i className="material-icons">delete</i>
-            </button>
-        </div>
-    );
+        this.state = {
+            editing: false
+        }
+    }
+
+    renderDisplay(){
+        return(
+            <div className={`todo ${this.props.completed ? 'completed':''}`}>   
+                <Checkbox checked={this.props.completed} onChange={() => this.props.onStatusChange(this.props.id) } />
+                <span className="todo-title">{this.props.title}</span>
+                <Button className="edit icon" icon="edit" onClick={}/>
+                <Button className="delete icon" icon="delete" onClick={() => this.props.onDelete(this.props.id)} />
+            </div>
+        );
+    }
+
+    renderForm(){
+        return(
+            <form className="todo-edit-form" onSubmit={this.handleSubmit} > 
+                <input type="text" ref="title" defaultValue={this.props.title} />
+                <Button className="save icon" icon="save" type="submit" />
+            </form>
+        );
+    }
+
+
+    render(){
+        return this.state.editing ? this.renderForm() : this.renderDisplay();  
+    }
 }
+
+
 
 Todo.propTypes = {
     title: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
+    completed: PropTypes.bool.isRequired,
+    onStatusChange: PropTypes.func.isRequired
 }
 
 export default Todo;
